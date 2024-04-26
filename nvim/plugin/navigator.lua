ceecb4d29af4ebf13a87ab8a7ea0223743d37392
Nikita Talalai <nikita.talalai.2002@gmail.com>
2024-04-26 22:57:58 +0300

    fix: disable navigation when pane is zoomed

--- nvim/plugin/navigator.lua
+++ nvim/plugin/navigator.lua
@@ -1,4 +1,7 @@
-require('Navigator').setup()
+require('Navigator').setup({
+  auto_save = 'current',
+  disable_on_zoom = true,
+})
 
 vim.keymap.set({'n', 't'}, '<M-h>', '<CMD>NavigatorLeft<CR>')
 vim.keymap.set({'n', 't'}, '<M-l>', '<CMD>NavigatorRight<CR>')
